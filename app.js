d76b2fcdf5563689abeaaca05c065e72cd2cca0c
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-09 13:57:24 -0600

    working on server

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
+var muppets = require('./routes/muppets');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var muppets = require('./routes/muppets')
-
 app.use('/api/muppets', muppets);
 
 app.listen(process.env.PORT || 8080);
--- routes/muppets.js
+++ routes/muppets.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.status(200).json({ message: 'muppets route was successful!' });
+  res.status(200).json({ muppet: 'Kermit' });
 });
 
 module.exports = router
