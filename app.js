e1d80b471b87cc977cb1c7e7c736e31e77670b7a
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-09 14:21:43 -0600

    CORS working

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require('dotenv').load()
+
 var express    = require('express');
 var bodyParser = require('body-parser');
 var muppets = require('./routes/muppets');
+var cors = require('cors');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/muppets', muppets);
 
+
 app.listen(process.env.PORT || 8080);
-console.log('Yay! The server started');
+console.log('Yay! The server started on port 8080');
